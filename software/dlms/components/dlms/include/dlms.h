9832821163bfbafdb575d226ff1af4c9cb5508d2
Fabian Plaimauer <f.plaimauer@gmail.com>
2023-08-06 01:54:48 +0200

    MBUS-Layer-Handling added

--- software/dlms/components/dlms/include/dlms.h
+++ software/dlms/components/dlms/include/dlms.h
@@ -17,14 +17,45 @@ extern "C" {
 /* ===== DLMS CHIP CONFIGURATION ===== */
 #define POWER_FAIL_SIGNAL_GPIO          21
 
+/* ===== GENERAL CONFIGURATION ===== */
+#define DATA_BUFFER_SIZE                1024            /* < General buffer size for data handling */
+
 /* ===== UART CONFIGURATION ===== */
 #define UART_PORT_NUMBER                UART_NUM_1
 #define UART_RX_GPIO                    22
 #define UART_TX_GPIO                    23
-#define UART_BUFFER_SIZE                1024
 #define UART_BAUD_RATE                  2400
 
-#define UART_EVENT_BUFFER_SIZE          (UART_BUFFER_SIZE)
+/* === M-BUS PARSER CONFIGURATION === */
+#define MBUS_MAX_SIZE                   256
+
+#define MBUS_HEADER_LENGTH              7
+#define MBUS_FOOTER_LENGTH              2
+
+#define MBUS_START_VALUE                0x68
+#define MBUS_START1_OFFSET              0
+#define MBUS_START2_OFFSET              3
+
+#define MBUS_LENGTH1_OFFSET             1
+#define MBUS_LENGTH2_OFFSET             2
+
+#define MBUS_USER_DATA_SIZE_OFFSET      3           /* < C-Field, A-Field and CI-Field */
+#define MBUS_USER_DATA_OFFSET           7
+
+#define MBUS_STOP_OFFSET                2
+#define MBUS_STOP_VALUE                 0x16
+
+/* ===== DLMS CONFIGURATION ===== */
+#define DLMS_MAX_SIZE             (MBUS_MAX_SIZE - MBUS_HEADER_LENGTH - MBUS_FOOTER_LENGTH)
+
+/* This part can be different for other smartmeters */
+#define DLMS_FRAME_COUNTER_OFFSET       15
+#define DLMS_FRAME_COUNTER_SIZE         4
+
+#define DLMS_FIRST_FRAME_OFFSET         (DLMS_FRAME_COUNTER_OFFSET + DLMS_FRAME_COUNTER_SIZE)
+#define DLMS_SECONDARY_FRAME_OFFSET     2
+
+#define GUE_KEY_LENGTH                  16
 
 /**
  * @brief Initialize uart and dlms
